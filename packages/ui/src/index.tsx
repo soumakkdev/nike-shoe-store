9685cb3d37ce4f3bcdbc13fd67035ac1b7cdcc39
soumakkdev <soumakkdutta@gmail.com>
2023-10-06 02:18:37 +0530

    shadcn ui, dashboard layout added

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export * from '@/components/ui/label'
+export * from '@/components/ui/input'
+export * from '@/components/ui/dialog'
+export * from '@/components/ui/dropdown-menu'
+export * from '@/components/ui/tooltip'
+export * from '@/components/ui/sheet'
+
+export * from '@/lib/utils'
